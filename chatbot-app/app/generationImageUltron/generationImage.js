0486f56a173b1770d87e1b7fa63d0b80f4f1f6dd
juliesgd05 <julie.segard05@gmail.com>
2024-10-28 19:23:46 +0100

    toute les trois seconde ca change et c'est fonctionnelle reelement

--- chatbot-app/app/generationImageUltron/generationImage.js
+++ chatbot-app/app/generationImageUltron/generationImage.js
@@ -2,41 +2,42 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 export default function ImageUltron() {
-  const images = [
-    '/Ultron_A.png',
-    '/ultron_consonne.png',
-    '/Ultron_I.png',
-    '/Ultron_neutre.png',
-    '/Ultron_O.png',
-    '/Ultron_vide.png',
-  ];
+    const images = [
+        '/ImageBlablaUltron2d/Ultron_A.png',
+        '/ImageBlablaUltron2d/ultron_consonne.png',
+        '/ImageBlablaUltron2d/Ultron_I.png',
+        '/ImageBlablaUltron2d/Ultron_neutre.png',
+        '/ImageBlablaUltron2d/Ultron_O.png',
+        '/ImageBlablaUltron2d/Ultron_vide.png',
+    ];
 
-  const [imageSrc, setImageSrc] = useState(images[0]);
+    const [imageSrc, setImageSrc] = useState(images[0]);
 
-  useEffect(() => {
-    const updateImage = () => {
-      const currentSeconds = new Date().getSeconds();
-      const index = Math.floor(currentSeconds / 3) % images.length;
-      setImageSrc(images[index]);
-    };
+    useEffect(() => {
+        const updateImage = () => {
+            const currentSeconds = new Date().getSeconds();
+            const index = Math.floor(currentSeconds / 3) % images.length;
+            setImageSrc(images[index]);
+        };
 
-    updateImage(); // Appel initial
-    const intervalId = setInterval(updateImage, 3000); // Mettre à jour toutes les 3 secondes
+        updateImage(); // Appel initial
+        const intervalId = setInterval(updateImage, 3000); // Mettre à jour toutes les 3 secondes
 
-    return () => clearInterval(intervalId); // Nettoyage à la désinstallation
-  }, []);
+        return () => clearInterval(intervalId); // Nettoyage à la désinstallation
+    }, []);
 
-  return (
-    <div>
-      <h1>Image qui change toutes les 3 secondes</h1>
-      {imageSrc && (
-        <Image 
-          src={imageSrc}
-          alt="Image changeante"
-          width={500}
-          height={300}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Image qui change toutes les 3 secondes</h1>
+            {imageSrc && (
+                <Image
+                    src={imageSrc}
+                    // src= '/ImageBlablaUltron2d/Ultron_A.png'
+                    alt="Image changeante"
+                    width={500}
+                    height={300}
+                />
+            )}
+        </div>
+    );
 }
